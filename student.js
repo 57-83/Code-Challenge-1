2661e1593101411d22c14bb0accbca838c52b356
+ <+>
2022-12-01 13:58:26 +0300

    done student.js

--- student.js
+++ student.js
@@ -1,5 +1,7 @@
-//Write that prompts the user to input student marks. The input should be between 0 and 100. Then output the correct grade:
-//A > 79, B - 60 to 79 C -  59 to 49, D - 40 to 49, E - less 40.
+///Write that prompts the user to input student marks.
+//The input should be between 0 and 100.
+//Then output the correct grade:
+
 const prompt = require("prompt-sync")();
 var marks = prompt("Enter your marks");
 //marks between 79 and 100
