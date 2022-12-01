5e4e9833e5ea893a0dfdc90aa70b981605a198ac
+ <+>
2022-12-01 13:59:18 +0300

    done speed.js

--- speed.js
+++ speed.js
@@ -1,4 +1,4 @@
-
+//For speed above 70
 const prompt= require("prompt-sync")();
 var speed=prompt("Enter your speed")
 // If the speed is less than 70, it should print “Ok”.
@@ -9,5 +9,5 @@ else
 console.log("Points:"+((speed-70)/5));}
 //if the driver gets more than 12 points, the function should print: “License suspended”.
 if((speed-70)/5>12){
-console.log(LicenseSuspended);
+console.log("LicenseSuspended");
 }
